707031cf37ccac67bf814669db7ffc2ca962acaf
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-14 17:24:03 +0600

    import user route

--- app.js
+++ app.js
@@ -3,12 +3,14 @@ const app = express();
 const cors = require("cors");
 
 // import necessery routes 
-
+const userRoute = require("./routes/user.route")
 
 // middleware
 app.use(express.json());
 app.use(cors());
 
+// using user routes
+app.use("/api/v1/users",userRoute)
 
 
 
