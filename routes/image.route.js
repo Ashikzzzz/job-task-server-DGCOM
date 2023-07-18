c362535c85137d2cc0840aae8cf3e759ac3711d4
ashik.emon5.im@gmail.com <ashik.emon5.im@gmail.com>
2023-07-18 17:41:22 +0600

    image controller

---
+++ routes/image.route.js
@@ -0,0 +1,9 @@
+const express = require("express")
+const router = express.Router()
+
+
+// get confirmation gmail route ---------
+router.route("/getting-image-base64-formate")
+.get()
+
+module.exports = router;
